ba85f2444d4c8f98e3baf3d5e25993dc15bd9f0e
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2025-02-20 20:49:36 +0900

    make func set time and wikipedia data

--- js/frontend.js
+++ js/frontend.js
@@ -1,9 +1,11 @@
 // EXPLAIN:制限時間設定関数、関数発火時設定秒数とalarm apiの作成をする。
-const timer = () => {
-  const endTime = Date.now() + 65 * 1000; // 現在時刻 + X秒
+const timer = (time = 60) => {
+  const intedTime = Number(time)
+  const endTime = Date.now() + intedTime * 1000; // 現在時刻 + X秒
+  const delayInMinutes = intedTime / 60 
   // 終了時刻を保存
   chrome.storage.local.set({ endTime: endTime }, () => {
-    chrome.alarms.create("timer", { delayInMinutes: 1 });
+    chrome.alarms.create("timer", { delayInMinutes: delayInMinutes });
   });
 };
 
@@ -20,26 +22,28 @@ document.addEventListener("DOMContentLoaded", async () => {
       const endtime = result.endTime;
       const remainingTime = endtime ? Math.max(0, Math.floor((endtime - Date.now()) / 1000)) : null;
       let tempCurrentWord = "";
-
       if (wordList.length > 0) {
         for (const word of wordList) {
           document.getElementById("chainWords").textContent += `${word} =>`;
           tempCurrentWord = word;
         }
       }
-
       if (word_start && word_end && number_of_steps) {
         document.getElementById("word_start").textContent = `start word : ${word_start}`;
         document.getElementById("word_end").textContent = `end word : ${word_end}`;
         document.getElementById("number_of_steps").textContent = `Your limitation Count : ${number_of_steps}`;
         document.getElementById("catch").setAttribute("disabled", true);
+        document.getElementById("second").setAttribute("disabled", true);
+        document.getElementById("url").setAttribute("disabled", true);
       }
 
       document.getElementById("count").textContent = `Your Current Count : ${count}`;
       
-      if ((wordList.length > 0 && Number(document.getElementById("count").textContent) >= number_of_steps) ||
+      if ((wordList.length > 0 && Number(count) >= number_of_steps) ||
         (wordList.length > 0 && tempCurrentWord === word_end) || (remainingTime < 1)) {
         document.getElementById("catch").removeAttribute("disabled");
+        document.getElementById("second").removeAttribute("disabled");
+        document.getElementById("url").removeAttribute("disabled");
         chrome.storage.local.set({
           returnCheckVal : checkValue(word_end, wordList[wordList.length - 1]),
           resultValue : wordList.toString(),
@@ -51,7 +55,9 @@ document.addEventListener("DOMContentLoaded", async () => {
               "word_start",
               "word_end",
               "number_of_steps",
-              "endTime"
+              "endTime",
+              "selectedSecondOption",
+              "selectedURLOption"
             ],
             () => {
               document.getElementById("word_start").textContent = "Not started game!";
@@ -59,6 +65,7 @@ document.addEventListener("DOMContentLoaded", async () => {
               document.getElementById("number_of_steps").textContent = "Not set count";
               document.getElementById("count").textContent = "Not set count";
               document.getElementById("chainWords").textContent = "";
+              document.getElementById("remaining-time").textContent = "Not set time";
             }
           )
         }).then(() => {
@@ -167,21 +174,42 @@ function wikipediaPageOpen(val) {
     }
   });
 }
+document.getElementById("second").addEventListener("change", (e) => {
+  chrome.storage.local.set({
+    selectedSecondOption: e.target.value
+  }
+  );
+});
 
+document.getElementById("url").addEventListener("change", async(e) => {
+  const sanitizedVal = sanitizeHTML(e.target.value);
+  if(await checkWikipediaURL(sanitizedVal)) {
+    chrome.storage.local.set({
+      selectedURLOption: sanitizedVal
+    }
+    );
+  } else {
+    alert("入力情報が不正です。");
+    document.getElementById("url").value = "";
+  }
+})
 // EXPLAIN:始まりと終わりの言葉をランダムで取得して画面に表示する関数。ここでtimerとページ遷移関数を発火させる。
 document.getElementById("catch").addEventListener("click", async () => {
   const [randomTitle_start, randomTitle_end] = await getRandomWikipediaTitle();
   if (randomTitle_start && randomTitle_end) {
-    const startWord = `start word : ${randomTitle_start.title}`;
-    const endWord = `end word : ${randomTitle_end.title}`;
-    document.getElementById("word_start").textContent = startWord;
-    document.getElementById("word_end").textContent = endWord;
-    chrome.storage.local.set({
-      word_start: randomTitle_start.title,
-      word_end: randomTitle_end.title,
-      number_of_steps: randomNumberFunc(),
-    });
-    timer();
+    chrome.storage.local.get(["selectedURLOption","selectedSecondOption"], (data) => {
+      const startWord = `start word : ${randomTitle_start.title}`;
+      const endWord = `end word : ${data.selectedURLOption ? data.selectedURLOption : randomTitle_end.title}`;
+      document.getElementById("word_start").textContent = startWord;
+      document.getElementById("word_end").textContent = endWord;
+
+      chrome.storage.local.set({
+        word_start: randomTitle_start.title,
+        word_end: data.selectedURLOption ? data.selectedURLOption : randomTitle_end.title,
+        number_of_steps: randomNumberFunc(),
+      });
+      timer(data.selectedSecondOption ? data.selectedSecondOption : 60);
+    })
     await wikipediaPageOpen(randomTitle_start.title);
   } else {
     console.log("タイトルの取得に失敗しました。");
@@ -194,3 +222,23 @@ chrome.runtime.onMessage.addListener((message) => {
     window.close();
   }
 });
+
+function sanitizeHTML(input) {
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+  };
+  return String(input).replace(/[&<>"'`=\/]/g, (char) => map[char]);
+}
+
+async function checkWikipediaURL(data) {
+  const baseURL = `https://ja.wikipedia.org/wiki/${encodeURIComponent(data)}`;
+  const response = await fetch(baseURL);
+  return response.ok;
+}
