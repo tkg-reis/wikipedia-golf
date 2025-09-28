62c55cddf927d6e77bdc5dbf8315380b014b8c69
tkg-reis <106098767+tkg-reis@users.noreply.github.com>
2025-09-28 18:14:08 +0900

    Restore content script highlights and popup messaging

--- js/content.js
+++ js/content.js
@@ -1,119 +1,27 @@
-// content.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "switchDomEffect") {
-    if (!message.enabled) {
-      chrome.runtime.sendMessage({ action: "open_popup" });
-      applyDomEffect();
-    } else {
-      removeDomEffect();
-    }
-    sendResponse({ status: "ok" });
-  }
-});
+// EXPLAIN:DOMの読み込みが行われるごとに、background.jsにポップアップを自動で開くメッセージを送信する。
+chrome.runtime.sendMessage({ action: "open_popup" });
 
-function applyDomEffect() {
+// EXPLAIN:クリック可能なリンクをハイライト処理
+const highlightWikiLinks = () => {
   const enableClickLinks = document.querySelectorAll("a[href^='/wiki/']");
   enableClickLinks.forEach((includeLink) => {
-    if (includeLink.dataset.originalBackgroundColor === undefined) {
-      includeLink.dataset.originalBackgroundColor = includeLink.style.backgroundColor;
-    }
-    if (includeLink.dataset.originalBorderWidth === undefined) {
-      includeLink.dataset.originalBorderWidth = includeLink.style.borderWidth;
-    }
-    if (includeLink.dataset.originalBorderStyle === undefined) {
-      includeLink.dataset.originalBorderStyle = includeLink.style.borderStyle;
-    }
-    if (includeLink.dataset.originalBorderColor === undefined) {
-      includeLink.dataset.originalBorderColor = includeLink.style.borderColor;
-    }
-
     includeLink.style.backgroundColor = "yellow";
     includeLink.style.borderWidth = "1px";
     includeLink.style.borderStyle = "dotted";
     includeLink.style.borderColor = "black";
   });
+};
 
-  document
-    .querySelectorAll("a[href]:not(a[href^='/wiki/'])")
-    .forEach((excludeLink) => {
-      if (excludeLink.dataset.originalPointerEvents === undefined) {
-        excludeLink.dataset.originalPointerEvents = excludeLink.style.pointerEvents;
-      }
-      if (excludeLink.dataset.originalColor === undefined) {
-        excludeLink.dataset.originalColor = excludeLink.style.color;
-      }
-
-      if (excludeLink.parentElement) {
-        if (excludeLink.parentElement.dataset.originalBackgroundColor === undefined) {
-          excludeLink.parentElement.dataset.originalBackgroundColor = excludeLink.parentElement.style.backgroundColor;
-        }
-        excludeLink.parentElement.style.backgroundColor = "black";
-      }
-
-      excludeLink.style.pointerEvents = "none";
-      excludeLink.style.color = "black";
-    });
-}
-
-function removeDomEffect() {
-  const enableClickLinks = document.querySelectorAll("a[href^='/wiki/']");
-  enableClickLinks.forEach((includeLink) => {
-    if (includeLink.dataset.originalBackgroundColor !== undefined) {
-      includeLink.style.backgroundColor = includeLink.dataset.originalBackgroundColor;
-      delete includeLink.dataset.originalBackgroundColor;
-    } else {
-      includeLink.style.backgroundColor = "";
-    }
-
-    if (includeLink.dataset.originalBorderWidth !== undefined) {
-      includeLink.style.borderWidth = includeLink.dataset.originalBorderWidth;
-      delete includeLink.dataset.originalBorderWidth;
-    } else {
-      includeLink.style.borderWidth = "";
-    }
-
-    if (includeLink.dataset.originalBorderStyle !== undefined) {
-      includeLink.style.borderStyle = includeLink.dataset.originalBorderStyle;
-      delete includeLink.dataset.originalBorderStyle;
-    } else {
-      includeLink.style.borderStyle = "";
-    }
-
-    if (includeLink.dataset.originalBorderColor !== undefined) {
-      includeLink.style.borderColor = includeLink.dataset.originalBorderColor;
-      delete includeLink.dataset.originalBorderColor;
-    } else {
-      includeLink.style.borderColor = "";
+// EXPLAIN:指定リンク以外を背景色を黒くして除外処理
+const dimNonWikiLinks = () => {
+  document.querySelectorAll("a[href]:not(a[href^='/wiki/'])").forEach((excludeLink) => {
+    excludeLink.style.pointerEvents = "none";
+    excludeLink.style.color = "black";
+    if (excludeLink.parentElement) {
+      excludeLink.parentElement.style.backgroundColor = "black";
     }
   });
+};
 
-  document
-    .querySelectorAll("a[href]:not(a[href^='/wiki/'])")
-    .forEach((excludeLink) => {
-      if (excludeLink.dataset.originalPointerEvents !== undefined) {
-        excludeLink.style.pointerEvents = excludeLink.dataset.originalPointerEvents;
-        delete excludeLink.dataset.originalPointerEvents;
-      } else {
-        excludeLink.style.pointerEvents = "";
-      }
-
-      if (excludeLink.dataset.originalColor !== undefined) {
-        excludeLink.style.color = excludeLink.dataset.originalColor;
-        delete excludeLink.dataset.originalColor;
-      } else {
-        excludeLink.style.color = "";
-      }
-
-      const parentElement = excludeLink.parentElement;
-      if (parentElement) {
-        if (parentElement.dataset.originalBackgroundColor !== undefined) {
-          parentElement.style.backgroundColor = parentElement.dataset.originalBackgroundColor;
-          delete parentElement.dataset.originalBackgroundColor;
-        } else {
-          parentElement.style.backgroundColor = "";
-        }
-      }
-    });
-}
-
-
+highlightWikiLinks();
+dimNonWikiLinks();
